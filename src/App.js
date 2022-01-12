a23e6a52182672dc318a2ca5873806e980a1b962
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-01-12 14:14:25 +0900

    useState로 좋아요 늘리기 실습

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -8,6 +10,8 @@ function App() {
   let [글제목,글제목변경] = useState('남자 코트 추천');
   let [글제목2,글제목변경2] = useState(['남자 코트 추천','여자 코트 추천']);
   // 이렇게 state로 해야 html이 재렌더링이 된다
+
+  let [따봉, 따봉변경] = useState(0);
 
   let posts = '강남 고기 맛집';
   function 함수(){
@@ -28,12 +32,12 @@ function App() {
         <hr/>
       </div>
       <div className='list'>
-        <h3> { 글제목2[0] } </h3>
+        <h3> { 글제목2[0] } <span onClick={ 함수() }>♥</span> 0 </h3>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
       <div className='list'>
-        <h3> { 글제목2[1] } </h3>
+        <h3> { 글제목2[1] } <span onClick={ ()=>{ 따봉변경(따봉+1) } }>♥</span> {따봉}  </h3>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
