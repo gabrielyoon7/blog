c7f55b82470bb4a3199f83d0cf706fc9ad00457d
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-01-17 15:38:47 +0900

    주석

--- public/NameForm.js
+++ public/NameForm.js
@@ -19,9 +19,11 @@ class NameForm extends React.Component {
     render() {
       return (
         <form onSubmit={this.handleSubmit}>
+          {/* 2. onSubmit을 통해서 handleSubmit 함수로 전달해서 alert를 호출*/}
           <label>
             Name:
             <input type="text" value={this.state.value} onChange={this.handleChange} />
+            {/* 1. input의 onChange를 통해서 handleChange 함수를 건드려서 state를 바꿈*/}
           </label>
           <input type="submit" value="Submit" />
         </form>
