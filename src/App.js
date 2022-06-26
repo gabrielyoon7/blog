c9cbbece9e973bf6d38660ce867e3b3bff7c98b0
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-06-26 14:27:33 +0900

    블로그 글 작성용 푸쉬

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const hi = 'Hello World';
+
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  useEffect(()=>{
+    setInterval(()=>setTime(new Date().toLocaleTimeString()), 1000);
+  });
+  
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>{hi}</div>
+        <div>{time}</div>
       </header>
     </div>
   );
